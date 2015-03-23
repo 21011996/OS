3a31938cab2e3edc70b22f20910636f91e31bba8
21011996 <21011996@mail.ru>
2015-03-23 12:00:14 +0300

    i was dump

--- lib/helpers.c
+++ lib/helpers.c
@@ -28,7 +28,20 @@ ssize_t read_until(int fd, void *buf, size_t count, char delimiter) {
 }
 
 ssize_t read_(int fd, void *buf, size_t counter) {
-    return read_until(fd, buf, count, -1);
+    ssize_t tread = 0;
+    ssize_t bread = 0;
+    while (tread < counter) {
+        bread = read(fd, buf + tread, counter - tread);
+        if (bread <= 0) {
+            break;
+        } else {
+            tread += bread;
+        }
+    }
+    if (bread < 0) {
+        return -1;
+    }
+        return tread;
 }
 
 ssize_t write_(int fd, const void *buf, size_t counter) {
